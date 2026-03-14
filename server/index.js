bf99b10db64368b63b79d0d8903afbf8143654bd
Vivek <vivekbisht4320@gmail.com>
2026-03-14 22:17:32 +0530

    Added health route so that server could be waked

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const app = express();
 const {dbConnection} = require('./config/connection');
 const cors = require('cors');
-const authMiddleware = require('./middleware/authMiddleware');
 const userRoutes = require('./routes/userRoutes');
 const movieRoutes = require('./routes/movieRoutes');
 const theatreRoutes = require('./routes/theatreRoutes');
@@ -28,6 +27,10 @@ app.use('/api/screen',screenRoutes);
 app.use('/api/show',showRoutes);
 app.use('/api/banner',bannerRoutes);
 app.use('/api/booking',bookingRoutes);
+
+app.get("/health",(req,res)=>{
+  res.status(200).json({status:"ok"});
+})
 
 app.listen(process.env.PORT,()=>{
     console.log("Server has started running on port : " + process.env.PORT);
