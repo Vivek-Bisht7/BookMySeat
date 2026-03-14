9431605f8505eb0b81cb4047a2771b992aaff359
Vivek <vivekbisht4320@gmail.com>
2026-03-14 15:42:58 +0530

    Bug fixed

--- server/config/firebaseAdmin.js
+++ server/config/firebaseAdmin.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../secrets/serviceAccountKey.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
     credential:admin.credential.cert(serviceAccount),
